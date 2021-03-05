7bdade0daa7261e9f806d85283262ebccc2ecdca
LuisSuarezzz <68974375+LuisSuarezzz@users.noreply.github.com>
2021-03-04 21:44:47 -0600

    actualizacion de codigo

--- book-api.js
+++ book-api.js
@@ -5,23 +5,23 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-//Libros
+// Where we will keep books
 let books = [];
 
 app.use(cors());
 
-//configuracion body parser middleware
+// Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/book', (req, res) => {
     const book = req.body;
 
-    //...
+    // Output the book to the console for debugging
     console.log(book);
-    books.push(books);
+    books.push(book);
 
-    res.send('book is added to the database');
+    res.send('Book is added to the database');
 });
 
-app.listen(port, () => console.log('Hello world app listening on port ${port}!'));
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
